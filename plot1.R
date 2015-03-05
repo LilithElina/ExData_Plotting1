5eec943dda363430e0e20ba735aafefb4945beb6
LilithElina <sarah.pohl@outlook.de>
2015-03-05 15:12:12 +0100

    Finished Assignment 1
    Added the four required plots as PNG, plus the R scripts that created
    them (with corresponding names).

---
+++ plot1.R
@@ -0,0 +1,23 @@
+# Set language to English for exact copy of the example plot
+Sys.setlocale("LC_TIME", "C")
+
+# Read in the relevant data (Feb 1st and 2nd in 2007)
+data <- read.csv("household_power_consumption.txt", header=FALSE, sep=";", skip=66637, nrow=2880)
+
+# Set comprehensive column names
+colnames(data) <- c("date", "time",
+                    "global_active_power", "global_reactive_power",
+                    "voltage", "global_intensity",
+                    "sub_metering1", "sub_metering2", "sub_metering3")
+
+# Merge date and time column
+data$datetime <- paste(data$date, data$time)
+
+# Convert date and time format
+data$datetime <- strptime(data$datetime, format="%d/%m/%Y %H:%M:%S")
+
+# Create histogram and ave as PNG
+png("plot1.png", width=480, height=480, units="px")
+hist(data$global_active_power, col="red",
+     main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,42 @@
+# Set language to English for exact copy of the example plot
+Sys.setlocale("LC_TIME", "C")
+
+# Read in the relevant data (Feb 1st and 2nd in 2007)
+data <- read.csv("household_power_consumption.txt", header=FALSE, sep=";", skip=66637, nrow=2880)
+
+# Set comprehensive column names
+colnames(data) <- c("date", "time",
+                    "global_active_power", "global_reactive_power",
+                    "voltage", "global_intensity",
+                    "sub_metering1", "sub_metering2", "sub_metering3")
+
+# Merge date and time column
+data$datetime <- paste(data$date, data$time)
+
+# Convert date and time format
+data$datetime <- strptime(data$datetime, format="%d/%m/%Y %H:%M:%S")
+
+# Create plots and save as PNG
+png("plot4.png", width=480, height=480, units="px")
+
+# Set multi-panel plotting
+par(mfrow=c(2,2))
+
+# First plot
+plot(data$datetime, data$global_active_power, type="l",
+     ylab="Global Active Power", xlab="")
+# Second plot
+plot(data$datetime, data$voltage, type="l",
+     ylab="Voltage", xlab="datetime")
+# Third plot
+plot(data$datetime, data$sub_metering1, type="l",
+     ylab="Energy sub metering", xlab="")
+lines(data$datetime, data$sub_metering2, type="l", col="red")
+lines(data$datetime, data$sub_metering3, type="l", col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col=c("black", "red", "blue"), lwd=1, bty="n")
+#Fourth plot
+plot(data$datetime, data$global_reactive_power, type="l",
+     ylab="Global_reactive_power", xlab="datetime")
+
+dev.off()
